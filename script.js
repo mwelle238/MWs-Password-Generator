610ec1d0772ea479246caec6278eafb56cccafb5
Michael Welle <michaelpwelle@gmail.com>
2023-01-04 19:12:44 -0800

    password generator functionally works, need to clean up

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Assignment code here
 function generatePassword() {
+  // get password criteria
   var needsLower = confirm("Do you need at least 1 Lowercase letter?");
   var needsUpper = confirm("Do you need at least 1 Uppercase letter?");
   var needsNumber = confirm("Do you need at least 1 number?");
@@ -10,33 +11,62 @@ function generatePassword() {
     var passwordLength = prompt("Enter password length 8-128:");
   } while (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128)
   
+  // have console display criteria
   console.log(needsLower);
   console.log(needsUpper);
   console.log(needsNumber);
   console.log(needsSpecial);
   console.log(passwordLength);
 
+  // define character sets
   var lowers = 'abcdefghijklmnopqrstuvwxyz';
   var uppers = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   var nums = '0123456789';
   var specialChars = '!@#$%^&*()-_=+?<>.,';
   var allChars = lowers + uppers + nums + specialChars;
   
-  console.log(allChars);
+  /* I will be adding 1 of each of the required characters to the beginning of the pwd string, 
+   then filling it with random characters from all sets
+   then randomizing the order of the string */
   
-  for (var i = 0; i < passwordLength; i++){
+  // adding required characters
+  if (needsLower)
+    pwd += lowers.charAt(Math.floor(Math.random()*lowers.length));
+  if (needsUpper)
+    pwd += uppers.charAt(Math.floor(Math.random()*uppers.length));
+  if (needsNumber)
+    pwd += nums.charAt(Math.floor(Math.random()*nums.length));
+  if (needsSpecial)
+    pwd += specialChars.charAt(Math.floor(Math.random()*specialChars.length));
+
+  while(pwd.length < passwordLength){
     pwd += allChars.charAt(Math.floor(Math.random()*allChars.length));
   }
 
+  // log pwd in console
   console.log(pwd);
 
-  
-     
-
-
+  // randomize pwd
+  var scramble = function (x) {
+    var ss = '';
+    var rand = 0;
+    while (x.length > 0) {
+      tmp = '';
+      rand = Math.floor(Math.random()*x.length);
+      ss += x.charAt(rand);
+      for (var i = 0; i<x.length; i++) {
+        if (i != rand) {
+          tmp += x.charAt(i);
+        }
+      }
+      x = tmp; 
+    }
+    console.log(ss);
+    return ss;
   }
- 
-  return pwd;
+
+  return scramble(pwd);
+  
 }
 
 // Get references to the #generate element
